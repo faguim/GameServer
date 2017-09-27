a360b0c4d9278ebaab8f4f3658bb140531814615
Francisco <francisnardi@gmail.com>
2017-09-27 18:49:48 -0300

    testing new

--- src/model/Case.java
+++ src/model/Case.java
@@ -7,8 +7,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Case {
-	private String conclusion_won_text = "Congratulations";
-	private String conclusion_lost_text = "Sorry, you have lost the game";
+	private String conclusion_won_text = "CONGRATULATIONS: YOU WON! :)";
+	private String conclusion_lost_text = "GAME OVER: YOU LOST! :P";
 	
 	private List<Transition> transitions = new ArrayList<>();
 	private List<Question> questions = new ArrayList<>();
