c9bc53ac18b2a47fd09cafa607e59e4af494183a
Fagner <pantoja.ti@gmail.com>
2017-10-20 17:53:24 -0200

    Adicionando método rest retornando jsonP

--- src/control/RestService.java
+++ src/control/RestService.java
@@ -2,9 +2,12 @@ package control;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.json.JSONWithPadding;
 
 import dao.MedicalCaseDAO;
 import model.MedicalCase;
@@ -15,12 +18,22 @@ public class RestService {
 	MedicalCaseDAO caseDAO = new MedicalCaseDAO();
 	
 	@GET
-	@Path("/case")
+	@Path("/casejsonp")
 	@Produces(MediaType.APPLICATION_JSON)
-	public MedicalCase getCase() {
+	public MedicalCase getCaseJSONP() {
 		Integer id = 1;
 		
 		MedicalCase medicalCase = caseDAO.findById(MedicalCase.class, id);
 		return medicalCase;
+	}
+	
+	@GET
+	@Path("/case")
+    @Produces({ "application/x-javascript", MediaType.APPLICATION_JSON + ";charset=utf-8" })
+	public JSONWithPadding getCase(@QueryParam("callback") String callback) {
+		Integer id = 1;
+		
+		MedicalCase medicalCase = caseDAO.findById(MedicalCase.class, id);
+        return new JSONWithPadding(new GenericEntity<MedicalCase>(medicalCase) {}, callback);
 	}
 }
